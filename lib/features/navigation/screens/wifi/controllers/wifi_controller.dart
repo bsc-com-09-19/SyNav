5afee36487b715631c4f73ab784423080692b742
isulu <bsc-com-09-19@unima.ac.mw>
2024-05-05 12:34:33 +0200

    update files

--- lib/features/navigation/screens/wifi/controllers/wifi_controller.dart
+++ lib/features/navigation/screens/wifi/controllers/wifi_controller.dart
@@ -1,32 +1,3 @@
-// import 'package:get/get.dart';
-// import 'package:flutter/services.dart';
-
-// class WifiController extends GetxController {
-//   // Creating a MethodChannel linked to the specified channel name.
-//   static const platform = MethodChannel('com.example.wifi/wifi');
-
-//   // RxList to hold the Wi-Fi networks (SSID and RSSI).
-//   var wifiList = <String>[].obs;
-
-//   // Asynchronous method to fetch the Wi-Fi list via the MethodChannel.
-//   Future<void> getWifiList() async {
-//     try {
-//       // Invoke the 'getWifiList' method on the native side and cast the result to List<String>.
-//       final List<dynamic> result = await platform.invokeMethod('getWifiList');
-//       wifiList.value = result.cast<String>();
-//     } on PlatformException catch (e) {
-//       // Handle the case where the platform method does not succeed.
-//       wifiList.value = ["Failed to get Wi-Fi list: '${e.message}'"];
-//     }
-//   }
-
-//   @override
-//   void onInit() {
-//     super.onInit();
-//     getWifiList();
-//   }
-// }
-
 
 import 'package:get/get.dart';
 import 'package:flutter/services.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sy_nav/bindings/home_binding.dart';
@@ -6,15 +8,24 @@ import 'package:sy_nav/features/navigation/screens/home/home.dart';
 import 'package:sy_nav/features/navigation/screens/navigation/navigationScreen.dart';
 import 'package:sy_nav/features/navigation/screens/nofications/notifications_screen.dart';
 import 'package:sy_nav/features/navigation/screens/wifi/wifi_screen.dart';
+import 'package:sy_nav/firebase_options.dart';
 import 'package:sy_nav/utils/themes/theme.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  ///Initializing the firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  ///Enables local caching
+  FirebaseDatabase.instance.setPersistenceEnabled(true);
   runApp(SyNavApp());
 }
 
 class SyNavApp extends StatelessWidget {
   SyNavApp({super.key});
+
+  ///Routes for navigation through the app
   final List<GetPage> appRoutes = [
     GetPage(
       name: '/', // Home screen as the initial route
