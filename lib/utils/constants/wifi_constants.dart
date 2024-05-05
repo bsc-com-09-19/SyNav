6e6cedc01aa47a834b400de3f7c9c911fb31dbf2
isulu <bsc-com-09-19@unima.ac.mw>
2024-05-05 16:19:46 +0200

    distance estimation using loss function

---
+++ lib/utils/constants/wifi_constants.dart
@@ -0,0 +1,7 @@
+class WifiConstants {
+  // Define path loss model parameters
+  static const referenceSignalStrength =
+      -30.0; // Reference signal strength at reference distance (in dBm)
+  static const referenceDistance = 1.0; // Reference distance (in meters)
+  static const pathLossExponent = 2.0; // Path loss exponent
+}
---
+++ lib/utils/helpers/wifi_algorithms.dart
@@ -0,0 +1,43 @@
+import 'dart:math';
+import 'package:sy_nav/features/navigation/screens/wifi/model/wifi_network.dart';
+import 'package:sy_nav/utils/constants/wifi_constants.dart';
+
+class WifiAlgorithms {
+  // Function to estimate distance based on signal strength using path loss model
+  static double estimateDistance(double signalStrength) {
+    return WifiConstants.referenceDistance *
+        pow(
+            10,
+            ((WifiConstants.referenceSignalStrength - signalStrength) /
+                (10 * WifiConstants.pathLossExponent)));
+  }
+}
+
+Point<double> dynamicWeightedTrilateration(
+    List<AccessPoint> apList, List<double> distances) {
+  if (apList.length < 3 && apList.length == distances.length) {
+    throw Exception(
+        "Number of access points is supposed to be 3 and has to be of same size with the distances array!!!");
+  }
+  // Constants (you can adjust these based on your scenario)
+  const double weight2_4GHz = 0.7; // Weight for 2.4 GHz band
+  const double weight5GHz = 0.3; // Weight for 5 GHz band
+
+  // Calculate weighted average of AP coordinates
+  double totalX = 0;
+  double totalY = 0;
+  double totalWeight = 0;
+
+  for (int i = 0; i < apList.length; i++) {
+    final weight = apList[i].frequency == '5GHz' ? weight5GHz : weight2_4GHz;
+    totalX += distances[i] * weight * apList[i].longitude;
+    totalY += distances[i] * weight * apList[i].latitude;
+    totalWeight += weight;
+  }
+
+  final estimatedX = totalX / totalWeight;
+  final estimatedY = totalY / totalWeight;
+
+  return Point(estimatedX, estimatedY);
+}
+
